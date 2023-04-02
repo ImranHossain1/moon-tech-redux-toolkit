b4f6e9d1a242537403ef3086d799e7304f24ddd4
ImranHossain1 <imranhossain1402@gmail.com>
2023-04-02 23:19:49 +0200

    redux toolkit added

---
+++ src/app/store.js
@@ -0,0 +1,16 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import cartSlice from "../features/cart/cartSlice";
+import filterSlice from "../features/filter/filterSlice";
+import logger from "redux-logger";
+import productsSlice from "../features/products/productsSlice";
+
+const store = configureStore({
+  reducer: {
+    cart: cartSlice,
+    filter: filterSlice,
+    products: productsSlice,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+});
+
+export default store;
